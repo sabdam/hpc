faacc6d4b7fce423977bc37c7740fb24e33e7065
Sabina Damirova <sab.damirova@gmail.com>
2020-09-21 19:00:16 +0300

    Created a parallel algorithm

--- lab6/lab6.cpp
+++ lab6/lab6.cpp
@@ -1,17 +1,22 @@
+#include <omp.h>
+#include <stdio.h>
 #include <iostream>
+#include <conio.h>
 
 #define N 1000
 
 bool isprime(int n)
 {
+  int c = 0;
   int d;
   if (n==1) 
     return false; 
   for (d=2; d*d<=n; d++)
   {
     if (n%d==0)
-      return false;
+      c++;
   } 
+  if (c>0) return false;
   return true;
 }
 
@@ -25,12 +30,30 @@ main()
       A[i] = i+1;  
     }
 
-  std::cout << "\n"; //Prime: ";
+  if (omp_get_max_threads() < 4)
+  {
+    printf("Мало нитей");
+    getch();
+    return(0);
+  }
+  else omp_set_num_threads(4);
   
-  for (int i=1; i<N; i++)
+  int i;
+  #pragma omp parallel for schedule (static, 250) private(i) shared(A) \
+    reduction(+:count)
+  for (i=1; i<N; i++)
   {
     if (isprime(A[i]) == true)
       count++;
   }
-  std::cout << count;
+
+  std::cout << "Number of primes: "<< count;
+  return(0);
 }
+
+
+
+ 
+
+
+

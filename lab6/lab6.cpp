082367cb3bf6a713550de01156e2a62f31aad6a3
sabdam <sab.damirova@gmail.com>
2020-09-26 17:40:48 +0300

    Added input of number of threads

--- lab6/lab6.cpp
+++ lab6/lab6.cpp
@@ -24,30 +24,36 @@ main()
 {
   int A[N];
   int count = 0;  
+  int nThreads;
   
   for (int i=1; i<N; i++)
     { 
       A[i] = i+1;  
     }
 
-  if (omp_get_max_threads() < 4)
-  {
-    printf("Мало нитей");
-    getch();
-    return(0);
-  }
-  else omp_set_num_threads(4);
+  std::cout << "Num threads: ";
+  std::cin >> nThreads;
+  if (nThreads > 0 && nThreads <= omp_get_max_threads())
+   {
+    omp_set_num_threads(nThreads);
   
-  int i;
-  #pragma omp parallel for schedule (static, 250) private(i) shared(A) \
-    reduction(+:count)
-  for (i=1; i<N; i++)
-  {
-    if (isprime(A[i]) == true)
-      count++;
+    int i;
+    #pragma omp parallel for schedule(static) private(i) shared(A) \
+      reduction(+:count)
+    for (i=1; i<N; i++)
+    {
+      if (isprime(A[i]) == true)
+        count++;
+    }
+
+    std::cout << "Number of primes: "<< count;
+  }
+  else
+  { 
+    printf("Incorrent input");
+    getch();
   }
 
-  std::cout << "Number of primes: "<< count;
   return(0);
 }
 
